1aee4f0dcbb293dfc7291a3de3027c461850df98
CristhianCami <59978429+CristhianCami@users.noreply.github.com>
2020-01-30 15:01:25 -0500

    Add files via upload

--- pruebas.py
+++ pruebas.py
@@ -17,6 +17,28 @@ class TestStringMethods(unittest.TestCase):
         a = (4,5)
         b = (3,8)
         self.assertEqual(multiplicacion(a,b), (-28,47))
+
+    def test_division(self):
+        a = (3,5)
+        b = (-3,-5)
+        self.assertEqual(division(a,b),(-1,0))
+
+    def test_modulo(self):
+        a = (3,4)
+        self.assertEqual(modulo(a),5)
+
+    def test_conjugado(self):
+        a = (3,-100)
+        self.assertEqual(conjugado(a),(3,100))
+
+    def test_conversion(self):
+        a = (10,5)
+        self.assertEqual(polaresACartesianas(cartesianasAPolares(a)),(10,5))
+
+    def test_fase(self):
+        a = (10,0)
+        self.assertEqual(fase(a),0)
+        
         
 
     
