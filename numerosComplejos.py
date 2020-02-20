10a55c4e277c15c021d10831884cdaf7f45da5a8
CristhianCami <59978429+CristhianCami@users.noreply.github.com>
2020-02-20 14:20:17 -0500

    Add files via upload

---
+++ numerosComplejos.py
@@ -0,0 +1,42 @@
+from sys import stdin
+import math
+
+
+def suma(r,t):
+    k = (r[0] + t[0], r[1] + t[1])
+    return k
+
+def resta(r,t):
+    k = (r[0] - t[0], r[1] - t[1])
+    return k
+
+def multiplicacion(r,t):
+    k = (r[0]*t[0] - r[1]*t[1],r[0]*t[1] + r[1]*t[0])
+    return k
+
+def division(r,t):
+    k = ((r[0]*t[0] + r[1]*t[1])/(t[0]**2+t[1]**2),(r[1]*t[0] - r[0]*t[1])/(t[0]**2+t[1]**2))
+    return k
+
+def modulo(r):
+    k = (r[0]**2 + r[1]**2)**(1/2)
+    return k
+
+def conjugado(r):
+    k = (r[0], -1*r[1])
+    return k
+
+def cartesianasAPolares(r):
+    l = (r[0]**2 + r[1]**2)**(1/2)
+    s = math.atan(r[1]/r[0])
+    return (l,s*(180/math.pi))
+
+def polaresACartesianas(e):
+    h = e[0]
+    c = e[1]*(math.pi/180)
+    return (h*math.cos(c),h*math.sin(c))
+
+def fase(c):
+    return math.atan2(c[1],c[0])
+
+
